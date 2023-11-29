70c5704312230174316b4ea4e7956d37ff30669f
saddansyah <saddanakbar@gmail.com>
2023-11-29 12:50:36 +0700

    re-init project

--- app/src/main/java/com/example/papb1/SimpalaRoom.kt
+++
@@ -1,8 +0,0 @@
-package com.example.papb1
-
-import android.app.Application
-import com.example.papb1.model.room.ReportDatabase
-
-class SimpalaRoom : Application() {
-    val reportDatabase by lazy { ReportDatabase.getDatabase(this) }
-}
